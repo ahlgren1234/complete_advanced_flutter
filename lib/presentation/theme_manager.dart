cfcbf35a37e33fb2d49c5bfffd69b995a3aba09a
Peter Ahlgren <ahlgren1234@gmail.com>
2023-01-11 11:39:47 +0100

    add card, appbar, button theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,7 @@
 import 'package:complete_advanced_flutter/presentation/color_manager.dart';
+import 'package:complete_advanced_flutter/presentation/font_manager.dart';
+import 'package:complete_advanced_flutter/presentation/styles_manager.dart';
+import 'package:complete_advanced_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
@@ -9,13 +12,49 @@ ThemeData getApplicationTheme() {
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager
         .grey1, // will be used in case of disabled button for example.
+    splashColor: ColorManager.primaryOpacity70,
     accentColor: ColorManager.grey,
 
     // card view theme
-
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
     // App bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
+    ),
 
     // Button theme
+    buttonTheme: ButtonThemeData(
+      shape: StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.primaryOpacity70,
+    ),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(
+          color: ColorManager.white,
+        ),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            AppSize.s12,
+          ),
+        ),
+      ),
+    ),
 
     // Text theme
 
