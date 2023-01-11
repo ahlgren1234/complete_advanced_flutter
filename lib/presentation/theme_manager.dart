2162c757fce2d774163fd5968014b6ac8669e5eb
Peter Ahlgren <ahlgren1234@gmail.com>
2023-01-11 11:46:02 +0100

    add text theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -35,7 +35,7 @@ ThemeData getApplicationTheme() {
 
     // Button theme
     buttonTheme: ButtonThemeData(
-      shape: StadiumBorder(),
+      shape: const StadiumBorder(),
       disabledColor: ColorManager.grey1,
       buttonColor: ColorManager.primary,
       splashColor: ColorManager.primaryOpacity70,
@@ -57,7 +57,22 @@ ThemeData getApplicationTheme() {
     ),
 
     // Text theme
-
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(
+        color: ColorManager.darkGrey,
+        fontSize: FontSize.s16,
+      ),
+      subtitle1: getMediumStyle(
+        color: ColorManager.lightGrey,
+        fontSize: FontSize.s14,
+      ),
+      caption: getRegularStyle(
+        color: ColorManager.grey1,
+      ),
+      bodyText1: getRegularStyle(
+        color: ColorManager.grey,
+      ),
+    ),
     // input decoration theme (text form field)
   );
 }
